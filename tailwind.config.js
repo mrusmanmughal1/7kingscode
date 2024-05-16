743d593acd01dcd1ea854da13646d85bef495fb4
Muhammad usman <mrusmanmughal1@gmail.com>
2024-05-17 01:25:35 +0500

    Tailwind Css Configured

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "blurr-bg": "#9d73ad",
+        primary: "#4e007a",
+      },
+    },
+  },
+  plugins: [],
+  darkMode: "class",
+};
