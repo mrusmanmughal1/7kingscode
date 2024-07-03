e0eb6d4bf8a7442f0a66e3964a2f99ef0d6ad9fe
sohaib bhatti <bhattisohaib11@gmail.com>
2024-07-04 02:20:36 +0500

    new updates

--- src/UI/BestSolution.jsx
+++ src/UI/BestSolution.jsx
@@ -4,61 +4,109 @@ import { NavLink } from "react-router-dom";
 import { RiBuilding2Line } from "react-icons/ri";
 import { TbDeviceMobileCog } from "react-icons/tb";
 import { GrShieldSecurity } from "react-icons/gr";
+import Slider from "react-slick";
 const BestSolution = () => {
   const [hover, setHover] = useState();
 
   const arr = [
     {
-      title: "Infrastructure Management",
-      desc: "Ensuring robust, efficient, and scalable IT infrastructure for seamless operations.",
+      title: "Telecommunications",
+      desc: " Advanced solutions to enhance connectivity and communication across networks.",
       icon: <RiBuilding2Line />,
       link: "/service/OnShoreOffShoreTeam",
     },
     {
-      title: " Application Management",
-      desc: " End-to-end services for optimizing, maintaining, and supporting your software applications..",
+      title: " Real Estate",
+      desc: "Comprehensive services for buying, selling, and managing properties efficiently..",
       icon: <TbDeviceMobileCog />,
       link: "/service/TalentAquisition",
     },
     {
-      title: "Security Services",
-      desc: "Comprehensive protection solutions to safeguard your assets and data. ",
+      title: "Finance and Banking",
+      desc: " Innovative solutions to optimize financial operations and banking services. ",
+      icon: <GrShieldSecurity />,
+      link: "/service/ManageConsult",
+    },
+    {
+      title: "Health Care",
+      desc: " Comprehensive services to enhance patient care and operational efficiency in healthcare settings.",
+      icon: <GrShieldSecurity />,
+      link: "/service/ManageConsult",
+    },
+    {
+      title: "Education",
+      desc: "Tailored solutions to foster learning and development through innovative educational practices. ",
       icon: <GrShieldSecurity />,
       link: "/service/ManageConsult",
     },
   ];
+  const settings = {
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
-    <div className="businessGrowth  md:">
-      <div className=" flex lg:flex-row flex-col container gap-10    w-11/12  lg:w-[80%] 2xl:w-3/4  py-20     mx-auto">
-        {arr.map((item, index) => (
-          <div
-            key={index}
-            onMouseOver={() => setHover(index)}
-            onMouseLeave={() => setHover(null)}
-            className="  overflow-hidden  w-[33%] hover:bg-black  space-y-2 bg-white shadow-md text-black hover:text-white  hover:-translate-y-8 transition-all duration-500   px-8 py-8"
-          >
-            <div className=" flex justify-between   items-center font-bold ">
-              <p className="hover:text-blue-secondary duration-500 transition-all  text-xl lg:text-[20px]   ">
-                {item.title}
-              </p>
-              <p
-                className={`${
-                  hover === index &&
-                  "before:bg-blue-900 before:opacity-100 text-white "
-                } text-5xl  text-blue-secondary relative   widget -top-4 best-icon `}
-              >
-                {item.icon}
-              </p>
-            </div>
-            <p
-              className={`${
-                hover === index && "text-white"
-              } text-gray-500  text-xl lg:text-[14px]  leading-7 `}
+    <div className="businessGrowth    ">
+      <div className=" pt-20  text-blue-secondary   pb-8 text-3xl w-[75%] mx-auto  capitalize font-semibold  ">
+        Industries We Serve at 7KC
+      </div>
+      <div className="  w-[80%]   pb-20  mx-auto">
+        <Slider {...settings}>
+          {arr.map((item, index) => (
+            <div
+              key={index}
+              onMouseOver={() => setHover(index)}
+              onMouseLeave={() => setHover(null)}
             >
-              {item.desc}
-            </p>
-          </div>
-        ))}
+              <NavLink key={index} to={item.link}>
+                <div
+                  className=" bg-white rounded-md text-black duration-700   h-48 mx-4 border border-white hover:bg-blue-secondary
+            shadow-lg space-y-2   hover:text-white leading-6 px-8 py-8"
+                >
+                  <div className="flex justify-between">
+                    <p className="font-bold text-xl">{item.title}</p>
+                    <p
+                      className={`text-5xl text-blue-secondary relative    -top-4 best-icon  ${
+                        hover == index && "text-white"
+                      } `}
+                    >
+                      {item.icon}
+                    </p>
+                  </div>
+                  <p className={``}>{item.desc}</p>
+                </div>
+              </NavLink>
+            </div>
+          ))}
+        </Slider>
       </div>
     </div>
   );
